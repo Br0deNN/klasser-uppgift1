494fcbc5869b54711b92d58505a77822c59053b6
7R1XY <theo.broden@gmail.com>
2024-04-03 10:37:52 +0200

    Add project files.

---
+++ klasser uppgift1/Program.cs
@@ -0,0 +1,21 @@
+﻿using klasser_uppgift1;
+using System;
+namespace uppgift1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            fordon bil = new fordon("BMW", "Sedan", "Svart", 2006, 13000);
+
+            Console.WriteLine(bil.märke);
+            Console.WriteLine(bil.modell);
+            Console.WriteLine(bil.färg);
+            Console.WriteLine(bil.årsmodell);
+            Console.WriteLine(bil.mil);
+
+
+
+        }
+    }
+}
---
+++ klasser uppgift1/fordon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasser_uppgift1
+{
+    class fordon
+    {
+        public string? märke;
+        public string? modell;
+        public string? färg;
+        public int? årsmodell;
+        public int? mil;
+
+        public fordon(string Mä, string mo, string fä, int år, int mi)
+        {
+            märke = Mä;
+            modell = mo;
+            färg = fä;
+            årsmodell = år;
+            mil = mi;
+        }
+
+    }
+}
